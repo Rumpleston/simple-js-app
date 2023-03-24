2cbeb2bf4e3c265b39c69b0d6f716fe2db370775
Jacob Miller <85430916+Rumpleston@users.noreply.github.com>
2023-03-24 16:05:02 -0500

    Updated scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,20 @@
-alert('Hello world');
+// Create an array of Pokemon objects, each object containing the name, height, and type(s) of a Pokemon
+let pokemonList = [{name:'Jigglypuff', height:'0.5', type:['Fairy, Normal'] },
+                    {name:'Vileplume', height:'1.2', type: ['Grass, Poison']},
+                    {name:'Beedrill', height:'1', type: ['Bug, Poison']},
+                    {name:'Fearow', height:'1.2', type:'Flying'},
+                    {name:'Raichu', height:'0.8', type:'Electric'},
+                    {name:'Haunter', height:'1.6', type:['Ghost, Poison']}]
+// Iterate through the pokemonList array
+ for (let i = 0; i < pokemonList.length; i++) {
+// Create a string containing the Pokemon's name and height as an HTML paragraph
+    let pokemonText = '<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')';
+// Check if the Pokemon's height is less than 0.6, and if so, add a comment about its size
+    if (pokemonList[i].height < 0.6) {
+         pokemonText += ' - that is a small Pokemon';
+    }
+// Close the paragraph HTML tag
+    pokemonText += '</p>';
+// Write the constructed paragraph with the Pokemon information to the document
+    document.write(pokemonText);
+}
